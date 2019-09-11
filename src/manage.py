5a5541a1ca66f28b39cb6ac129e9a99fcc3f649f
JamesBisese <james.bisese@tetratech.com>
2019-09-11 15:42:36 -0600

    edit to test github connec tion to PyCharm

--- src/manage.py
+++ src/manage.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     # CHANGED manage.py will use development settings by
     # default. Change the DJANGO_SETTINGS_MODULE environment variable
     # for using the environment specific settings file.
+
+    # edit to test GitHub only
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "costly.settings.production")
 
     from django.core.management import execute_from_command_line
